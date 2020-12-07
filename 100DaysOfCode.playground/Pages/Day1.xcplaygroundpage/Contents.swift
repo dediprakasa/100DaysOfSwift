30d81d5b6c6927a2638d8a62934338e71a7bd2d3
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-08 06:39:39 +0700

    tried 'decorated' numeric and multi line strings

---
+++ 100DaysOfCode.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var paddedDouble = 000123.345
+var bigNumber = 1_000_000
+
+print(paddedDouble)
+print(bigNumber)
+
+var multiLineText = """
+Line 1
+Line 2
+Line 3
+"""
+
+print(multiLineText)
