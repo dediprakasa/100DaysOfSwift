c017448de3c6cd5789809b94921c63e7486fe388
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-09 21:44:41 +0700

    added Set experiment - class doesnt support automatic hash generation

--- 100DaysOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ 100DaysOfCode.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -7,3 +7,33 @@ formatter.dateFormat = "dd-MM-yyyy"
 let result = formatter.string(from: date ?? Date())
 
 print(result)
+
+struct Obj: Hashable {
+    let name: String
+}
+
+// automatic synthesis of 'Hashable' is not supported for classes
+//class ObjClass: Equatable, Hashable {
+//    static func == (lhs: ObjClass, rhs: ObjClass) -> Bool {
+//        lhs.name == rhs.name
+//    }
+//
+//
+//    let name: String
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+
+let obj = Obj(name: "one")
+let obj2 = Obj(name: "one")
+
+//let room = ObjClass(name: "class")
+//let room2 = ObjClass(name: "class")
+
+let set = Set([obj, obj2])
+
+//let set2 = Set([room, room2])
+
+print(set.count)
