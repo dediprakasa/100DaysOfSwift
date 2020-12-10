9eae6104c65de42a1fb9a5bd3b02f7e3d866d3d1
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-10 13:44:51 +0700

    added matching optionals

--- 100DaysOfCode.playground/Pages/Day3.xcplaygroundpage/Contents.swift
+++ 100DaysOfCode.playground/Pages/Day3.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+struct Student {
+  let name: String?
+  let batch: Int?
+}
 
-//: [Next](@next)
+let dedi = Student(name: "Dedi", batch: 2015)
+
+func validateStudent(name: String?, batch: Int?) {
+  switch (name, batch) {
+    case let (.some(matchedName), .some(matchedBatch)):
+      print("Hello \(matchedName) from batch \(matchedBatch)")
+    case let (.some(matchedName), .none):
+      print("\(matchedName), please enter your batch!")
+    default:
+      print("Are you really a student?")
+  }
+}
+
+validateStudent(name: dedi.name, batch: dedi.batch)
+validateStudent(name: "", batch: 0)
