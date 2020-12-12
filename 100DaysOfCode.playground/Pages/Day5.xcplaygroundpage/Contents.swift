a7cb5804f5b2e0b258d10f5379aff7210a17a3a7
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-12 23:52:03 +0700

    added variadic function

---
+++ 100DaysOfCode.playground/Pages/Day5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+func defenderLineUp(players: String...) {
+    for player in players {
+        print(player)
+    }
+}
+
+defenderLineUp(players: "AWB", "Lindelof", "Maguire", "Shaw")
