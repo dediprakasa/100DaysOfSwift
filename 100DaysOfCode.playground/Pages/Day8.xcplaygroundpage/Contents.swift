e0c4976650e2f61cf31be5273fc258bf0ac64b6a
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-15 20:27:08 +0700

    added mutating func

--- 100DaysOfCode.playground/Pages/Day8.xcplaygroundpage/Contents.swift
+++ 100DaysOfCode.playground/Pages/Day8.xcplaygroundpage/Contents.swift
@@ -1,7 +1,18 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+struct Ibra {
+    var name: String
+    
+    mutating func changeName(to name: String) {
+        self.name = name
+    }
+}
 
-//: [Next](@next)
+let ibra = Ibra(name: "Ibra")
+
+// can't be executed
+//ibra.changeName(to: "Lingard")
+
+var arbi = Ibra(name: "Movic")
+arbi.changeName(to: "Bebe")
+print(arbi.name)
