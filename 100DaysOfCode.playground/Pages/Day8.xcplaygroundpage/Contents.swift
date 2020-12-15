18b1640d7cbe35e13c2b43003db5c4cffead1a9c
Dedi Prakasa <dedprakasa@gmail.com>
2020-12-15 20:45:47 +0700

    added struct initializer

--- 100DaysOfCode.playground/Pages/Day8.xcplaygroundpage/Contents.swift
+++ 100DaysOfCode.playground/Pages/Day8.xcplaygroundpage/Contents.swift
@@ -16,3 +16,20 @@ let ibra = Ibra(name: "Ibra")
 var arbi = Ibra(name: "Movic")
 arbi.changeName(to: "Bebe")
 print(arbi.name)
+
+// create custom initializer in extension to retain memberwise initializer
+extension Ibra {
+    init(heyho: String) {
+        name = heyho + "Uhuy"
+    }
+    
+    init() {
+        name = "Ibrahimovic"
+    }
+}
+
+let name1 = Ibra(name: "Ibracadabra")
+let name2 = Ibra(heyho: "Wooow")
+let name3 = Ibra()
+
+print(name1, name2, name3)
