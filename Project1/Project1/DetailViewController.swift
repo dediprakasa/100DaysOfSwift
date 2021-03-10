3f239ea13b8801d1d81b5cd0778f4d19dc930217
Dedi Prakasa <dedprakasa@gmail.com>
2021-03-11 00:30:48 +0700

    navigate and load detail

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var ImageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            ImageView.image = UIImage(named: imageToLoad)
+        }
     }
     
 
