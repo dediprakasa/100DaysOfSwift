da1c7213bc6d6042aed88ecdbe5bb6da3004b5fe
Dedi Prakasa <dedprakasa@gmail.com>
2021-03-22 22:50:45 +0700

    setup project5

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Dedi Prakasa on 3/22/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
